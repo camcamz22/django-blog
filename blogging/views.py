5d332b8aa2c9564c7b150658178ae6249ceb8f66
Camila Dominguez <camiladb@uw.edu>
2024-12-02 16:55:26 -0800

    Created CBV for blogging

--- blogging/views.py
+++ blogging/views.py
@@ -1,32 +1,36 @@
+from msilib.schema import ListView
+
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views import View
+
 from blogging.models import Post
 from django.template import loader
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args: \n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs: \n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class StubView(View):
+    def get(self, request, *args, **kwargs):
+        body = "Stub View\n\n"
+        if args:
+            body += "Args: \n"
+            body += "\n".join(["\t%s" % a for a in args])
+        if kwargs:
+            body += "Kwargs: \n"
+            body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+        return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date') # reversed order
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
 
+    def get_queryset(self, request, *args, **kwargs):
+        published = Post.objects.exclude(published_date__exact=None)
+        posts = published.order_by('-published_date')
+        context = {'posts': posts}
+        return render(request, 'blogging/list.html', context)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
